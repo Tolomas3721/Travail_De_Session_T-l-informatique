b8712d2ce9f04bc358d41601936bd266c5c727e9
Tolomas3721 <thomasbeaulieu3721@gmail.com>
2026-04-04 13:32:08 -0400

    setup

---
+++ Code/server.py
@@ -0,0 +1,13 @@
+from socket import AF_INET, SOCK_DGRAM
+from usocket import usocket
+
+s = usocket(
+    family=AF_INET,
+    type=SOCK_DGRAM,
+    fiabilite=0.85,
+    taux_corruption=0.05,
+)
+
+s.connect(("127.0.0.1", 9999))
+s.send(b"segment 1")
+reponse = s.recv(1024)
